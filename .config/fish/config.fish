7b621f4bcbb48275470eeb20c2f4f7b3f5fb331d
Rueshyna <rueshyna(XD)gmail.com>
2012-11-05 17:47:53 +0800

    add PROMPT_HOST into ENV

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,10 @@
 set -u CLASSPATH
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-set PATH $PATH /usr/texbin /opt/local/bin /opt/local/sbin /opt/local/share/man ~/bin
+set -x PATH $PATH /usr/texbin /opt/local/bin /opt/local/sbin /opt/local/share/man ~/bin
+set -x PROMPT_HOST (hostname)
+set -x PROMPT_SHELL fish
+
 
 function fish_prompt -d 'custom prompt'
   set -x TERM xterm-256color
